a368d20f9f825fa0ead7e89fd2ec2a09cadbb743
BW <19BWeng@keshacademy.com>
2025-03-19 18:55:31 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from pets import Pet 
+pet1 = Pet("Garfield", "Cat", "Yellow cat loves lasagne", "3", "male")
+pet2 = Pet("Bluey", "Dog", "Blue dog who likes to play", "2", "female")
+pet3 = Pet("Scooby-Doo", "Dog", "Easily scared and loves to solve mysteries", "5", "male")
+
+print(pet1.get_name())
+print(pet2.get_age())
+print(pet3.get_description())
